2912dcfc0e752b0a0b8964ca680af4a1163085e5
Killian Christopher Smyth <KILLIAN.SMYTH.2018@mumail.ie>
2022-02-15 12:16:06 +0000

    Agents now update in the UI using signals & slots

--- QCustomPlotTestVS/QCustomPlotTestVS/Bucket.h
+++ QCustomPlotTestVS/QCustomPlotTestVS/Bucket.h
@@ -22,6 +22,21 @@ extern std::vector<double> latestPowerConsumption;
 //alias for timetable datatype
 using Timetable = std::unordered_map<int, double>;
 
+class AgentUI : public QObject {
+	Q_OBJECT
+
+public:
+	AgentUI(QObject* parent = Q_NULLPTR) {};
+	~AgentUI();
+	void newAgent(std::string name, std::string type, double power, int index);
+	void setPower(std::string type, int index, double power);
+
+signals:
+	void addAgentToUI(QString name, QString type, double power, int index);
+	void powerChanged(QString type, int index, double power);
+};
+
+
 class Bucket {
 
 	public:
@@ -41,21 +56,4 @@ class Bucket {
 		void drainBattery(int intervals);
 		void suspendThread(int milliseconds);
 		int checkInterval(std::function<void(int)> callback, double amount, int lastAction);
-};
-
-class AgentUI : public QObject {
-	Q_OBJECT
-
-	public:
-		AgentUI(QObject* parent = Q_NULLPTR);
-		~AgentUI();
-		void newAgent(std::string name, std::string type, double power, int index);
-		void setPower(std::string type, int index, double power);
-
-	private:
-		int m_index;
-
-	signals:
-		void addAgentToUI(QString name, QString type, double power, int index);
-		void powerChanged(QString type, int index, double power);
 };
